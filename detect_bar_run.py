cd213a463bb6183b701a8e39fd1fa5a65e548a52
1234wkyl <68144213+1234wkyl@users.noreply.github.com>
2025-09-11 13:00:38 +1200

    Delete detect_bar_run.py

--- detect_bar_run.py
+++
@@ -1,6 +0,0 @@
-rows2 = run_detect(
-    input_path="./2015",
-    output_dir="./keogram-out2",
-    save_overlay=True, save_mask=True, save_inpaint=True,
-    csv_path="./keogram-out2/detections.csv"
-)
